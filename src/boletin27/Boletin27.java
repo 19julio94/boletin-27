3267f5bde3d98bc87050c21571d681968d77b522
Julio <juliopatriciodasilva@gmail.com>
2016-04-21 11:14:01 +0200

    final

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,16 @@
+
+package boletin27;
+
+/**
+ *
+ * @author JP
+ */
+public class Boletin27 {
+
+   
+    public static void main(String[] args) {
+       Ventana sj = new Ventana();
+       sj.setVisible(true);
+    }
+    
+}
